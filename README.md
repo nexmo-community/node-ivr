e130d3a3c9ace03c2bdb38891f3d0e214bc7fc2c
Mark Lewin <mark.lewin@vonage.com>
2021-01-28 15:34:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Build a call menu
+
+This example app shows you how to build a simple call menu (also known as an Interactive Voice Response (IVR)) using the Vonage Voice API and Node.js.
+
+Instructions for configuring running this application can be found in the [accompanying tutorial](https://developer.nexmo.com/voice/voice-api/tutorials/ivr/introduction/node).
---
+++ index.js
@@ -0,0 +1,59 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/webhooks/answer', (req, res) => {
+	res.json(mainMenu(req));
+});
+
+app.post('/webhooks/events', (req, res) => {
+	console.log(req.body);
+	res.sendStatus(204);
+});
+
+app.post('/webhooks/dtmf', (req, res) => {
+	let actions = [];
+	let ncco = [];
+	switch (req.body.dtmf) {
+		case '1':
+			actions.push({
+				action: 'talk',
+				text: `It is ${new Intl.DateTimeFormat(undefined, {
+					dateStyle: 'full',
+					timeStyle: 'long',
+				}).format(Date.now())}`,
+			});
+			break;
+		case '2':
+			actions.push({
+				action: 'stream',
+				streamUrl: [
+					'https://nexmo-community.github.io/ncco-examples/assets/voice_api_audio_streaming.mp3',
+				],
+			});
+	}
+	ncco = actions.concat(mainMenu(req));
+
+	console.log(ncco);
+
+	res.json(ncco);
+});
+
+function mainMenu (req) {
+	return [
+		{
+			action: 'talk',
+			bargeIn: true,
+			text:
+				'Welcome. Press 1 to hear the current date or 2 to play audio. Press any other key to hear these options again.',
+		},
+		{
+			action: 'input',
+			maxDigits: 1,
+			eventUrl: [ `${req.protocol}://${req.get('host')}/webhooks/dtmf` ],
+		},
+	];
+}
+
+app.listen(3000);
